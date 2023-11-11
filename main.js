7e446900fbfc5e91ac5d4bd082d44dce8a5f530d
jyni556 <devdesi10@gmail.com>
2023-11-11 19:51:51 +0900

    Add my name

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hello, BCS4");
+const myName = document.getElementById("my-name");
+
+const h3 = document.createElement("h3");
+
+h3.innerText = "My name is h662.";
+
+myName.appendChild(h3);
